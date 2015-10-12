eec4066693ced7bda387ba2d035de8661c94a77d
janszen2 <idris@janszen.biz>
2015-10-12 16:09:31 +0200

    incheckzuil

--- incheckzuil.py
+++ incheckzuil.py
@@ -1,17 +1,21 @@
 from tkinter import *
+import sqlite3
+conn = sqlite3.connect('database.db')
 
-def incheckzuil2():
+def invoer_incheckzuil():
     global e1
     master = Tk()
     Label(master, text="Voer ov-chipkaartnummer in").grid(row=0)
     e1 = Entry(master)
     e1.grid(row=0, column=1)
-    Button(master, text='Quit', command=master.quit).grid(row=3, column=0, sticky=W, pady=4)
-    Button(master, text='Show', command=Incheckzuil).grid(row=3, column=1, sticky=W, pady=4)
-    mainloop( )
+    Button(master, text='Invoeren', command=master.quit).grid(row=0, column=4 , sticky=W, pady=20)
+    mainloop()
+    f = e1.get()
+    return f
 
-def incheckzuil():
-    global e1
-    print("Ov-chipkaartnummer is " +  e1.get())
-
-incheckzuil()
+def vergelijk_database():
+    global f
+    global conn
+    for row in conn:
+        if f == row:
+            print( row[0])
