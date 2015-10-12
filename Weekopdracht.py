da35603a7edf57be1bf40425df6fc01bd7e069e0
Jalal <jalal.elmansouri@student.hu.nl>
2015-10-12 12:06:24 +0200

    Test

--- Weekopdracht.py
+++ Weekopdracht.py
@@ -9,7 +9,7 @@ ovnummer = int(input("Voer uw ov-chipkaartnummer: "))
 beginstation = input("Voer uw beginstation in: ")
 eindstation = input("Voer uw eindstation in: ")
 
-gegevens=naam,ovnummer,beginstation,eindstation
+gegevens = naam,ovnummer,beginstation,eindstation
 
 import qrcode
 def generateQR():
@@ -25,3 +25,4 @@ def generateQR():
     img = qr.make_image()
     img.show()
 generateQR()
+
