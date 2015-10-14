84c96c50f12a425c8385db685938f76f6da3f9c3
janszen2 <idris@janszen.biz>
2015-10-14 16:23:46 +0200

    Marketing tool

--- Incheckzuil & Marketingoverzicht.py
+++ Incheckzuil & Marketingoverzicht.py
@@ -1,39 +1,78 @@
 from tkinter import *
 import sqlite3
+from collections import Counter
+import collections
 
-
-def invoer_incheckzuil():
-    """
-    Invoer in de incheckzuil via TKinter
-    :return: invoer
-    """
-    TKintermenu = Tk()
-    Label(TKintermenu, text="Voer ov-chipkaartnummer in").grid(row=0)
-    invoerveld = Entry(TKintermenu)
-    invoerveld.grid(row=0, column=1)
-    Button(TKintermenu, text='Invoeren', command=TKintermenu.quit).grid(row=0, column=4 , sticky=W, pady=20)
-    mainloop()
-    invoer = invoerveld.get()
-    return invoer
-
-
-def vergelijk_database(invoer):
-    """
-    Vergelijkt invoer van de vorige functie in de database en aan de hand van de invoer informatie aan de gebruiker
-    :param invoer:
-    :return: None
-    """
+def connector():
     conn = sqlite3.connect('Reizigers.db')
     cursor = conn.cursor()
-    waardeinvoer = (invoer,)
-    databasewaarden = cursor.execute('SELECT * FROM ReizigersDB WHERE OVnummer =?', waardeinvoer)
+    return cursor
+
+# def invoer_incheckzuil():
+#     """
+#     Invoer in de incheckzuil via TKinter
+#     :return: invoer
+#     """
+#     TKintermenu = Tk()
+#     Label(TKintermenu, text="Voer ov-chipkaartnummer in").grid(row=0)
+#     invoerveld = Entry(TKintermenu)
+#     invoerveld.grid(row=0, column=1)
+#     Button(TKintermenu, text='Invoeren', command=TKintermenu.quit).grid(row=0, column=4 , sticky=W, pady=20)
+#     mainloop()
+#     invoer = invoerveld.get()
+#     return invoer
+
+
+# def vergelijk_database(invoer,cursor):
+#     """
+#     Vergelijkt invoer van de vorige functie in de database en aan de hand van de invoer informatie aan de gebruiker
+#     :param invoer:
+#     :return: None
+#     """
+#     waardeinvoer = (invoer,)
+#     databasewaarden = cursor.execute('SELECT * FROM ReizigersDB WHERE OVnummer =?', waardeinvoer)
+#     resultaten = databasewaarden.fetchall()
+#     teller = len(resultaten)
+#     print(teller)
+#     if teller == 0:
+#         print("Dit OVnummer is nog niet in onze database bekend.")
+#     for row in resultaten:
+#         print(row)
+
+
+
+def populairste_vertrekstation(cursor):
+    connector()
+    database_eindstation = cursor.execute('SELECT Eindstation FROM ReizigersDB')
+    eindstation = []
+    for row in database_eindstation:
+        eindstation += row
+    print("Het populairste vertrekstation is: " + max(eindstation))
+
+
+def populairste_bestemming(cursor):
+    connector()
+    database_beginstation = cursor.execute('SELECT Beginstation FROM ReizigersDB')
+    beginstation = []
+    for row in database_beginstation:
+        beginstation += row
+    print("De populairste bestemming is: " + max(beginstation))
+
+def aantal_reizen_per_ov(cursor):
+    databasewaarden = cursor.execute('SELECT OVnummer FROM ReizigersDB')
     resultaten = databasewaarden.fetchall()
-    teller = len(resultaten)
-    print(teller)
-    if teller == 0:
-        print("Dit OVnummer is nog niet in onze database bekend.")
+    ovnummer= []
     for row in resultaten:
-        print(row)
+        ovnummer += row
+    c = collections.Counter(ovnummer)
+    for letter in ovnummer:
+        print (letter, c[letter])
+    # print (Counter(ovnummer))
 
-vergelijk_database(invoer_incheckzuil())
 
+
+
+aantal_reizen_per_ov(connector())
+populairste_bestemming(connector())
+populairste_vertrekstation(connector())
+# vergelijk_database(invoer_incheckzuil(),connector())
