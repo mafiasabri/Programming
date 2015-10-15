6ffcf314d58619e0769708dee5f08890800a4a10
janszen2 <idris@janszen.biz>
2015-10-15 12:51:24 +0200

    WERKEND G

--- Incheckzuil & Marketingoverzicht.py
+++ Incheckzuil & Marketingoverzicht.py
@@ -43,29 +43,39 @@ def populairste_vertrekstation(cursor):
     eindstation = []
     for row in database_eindstation:
         eindstation += row
-    print("Het populairste vertrekstation is: " + max(eindstation))
+    populairste_vertrek = ("Het populairste vertrekstation is: " + max(eindstation))
+    return populairste_vertrek
 
 def populairste_bestemming(cursor):
     database_beginstation = cursor.execute('SELECT Beginstation FROM ReizigersDB')
     beginstation = []
     for row in database_beginstation:
         beginstation += row
-    print("De populairste bestemming is: " + max(beginstation))
+    populairste_best =("De populairste bestemming is: " + max(beginstation))
+    return populairste_best
 
 def aantal_reizen_per_ov(cursor):
     databasewaarden = cursor.execute('SELECT OVnummer FROM ReizigersDB')
     resultaten = databasewaarden.fetchall()
     ovnummer= []
+    minidatabase =[]
     for row in resultaten:
         ovnummer += row
     counter = collections.Counter(ovnummer)
     for letter in counter:
-        print (letter, counter[letter])
+        minidatabase += (letter, counter[letter])
+    return(str(minidatabase))
 
-aantal_reizen_per_ov(connector())
-print()
-populairste_bestemming(connector())
-populairste_vertrekstation(connector())
-print()
-vergelijk_database(invoer_incheckzuil(),connector())
+def schrijven_naar_txt(minidatabase, populairste_best, populairste_vertek):
+    file = open("Marketingverslag.txt","w")
+    file.writelines(minidatabase)
+    file.writelines(populairste_best)
+    file.writelines(populairste_vertek)
 
+aantal_reis = aantal_reizen_per_ov(connector())
+print()
+pop_best = populairste_bestemming(connector())
+pop_vert = populairste_vertrekstation(connector())
+print()
+DB_vergelijk = vergelijk_database(invoer_incheckzuil(),connector())
+schrijf_txt = schrijven_naar_txt(aantal_reis, pop_best, pop_vert)
